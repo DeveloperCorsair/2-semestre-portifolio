db396aa840799b2c8efebc503c6a39714730e00f
henrique.com <henriquemartins7t@gmail.com>
2026-05-08 10:41:07 -0300

    feat: Restauração de pastas

--- src/routes/contato.js
+++
@@ -1,8 +0,0 @@
-// routes/contato.js
-const express = require("express");
-const router = express.Router();
-const { enviarMensagem } = require("../controllers/contatoController");
-
-router.post("/", enviarMensagem);
-
-module.exports = router;
